bf213eb6d5d0bf58d32e6031bcb4bfb7d65ee845
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2021-07-26 10:32:26 +0200

    commit first go project

---
+++ README.md
@@ -0,0 +1,2 @@
+## First Golang Project
+This is my first go project. This program is a few function of a card game.
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+}
